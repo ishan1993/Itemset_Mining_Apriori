da6878c46f9a37b7d1260ac2063590583788dddf
Ishan Shah <ishan1993@gmail.com>
2019-01-21 18:43:47 -0800

    complete

--- Itemset_Mining_Apriori.py
+++ Itemset_Mining_Apriori.py
@@ -52,15 +52,11 @@ def gen_fk(ck):
 def gen_ckplus1(fk,x):
     for elem in fk:
         elem.remove(elem[0])
-    print("\nfk: " + str(len(fk)))
-    print (fk[0:5])
     f_items = []
     for elem in fk:
         f_items = f_items + elem
     f_items = set(f_items)
     f_items = list(f_items)
-    print("\nf_items: " + str(len(f_items)))
-    print (f_items[0:5])
     
     lkplus1 = list(combinations(f_items, len(fk[0])+1))
     i = 0
@@ -68,7 +64,6 @@ def gen_ckplus1(fk,x):
         lkplus1[i] = list(lkplus1[i])
         i=i+1
     print("\nlkplus1: " + str(len(lkplus1)))
-    print(lkplus1[0:5])
     
     #prune
     for elem in lkplus1:
@@ -89,7 +84,6 @@ def gen_ckplus1(fk,x):
     #dbscan
     ckplus1 = []
     print("\nlkplus1 after pruning: " + str(len(lkplus1)))
-    print(lkplus1[0:5])
     for elem in lkplus1:
         sup = 0
         for cat_list in tdb:
@@ -97,28 +91,19 @@ def gen_ckplus1(fk,x):
                 sup = sup + 1
         ckplus1.append([sup]+list(elem))
     print("\nckplus1: " + str(len(ckplus1)))
-    print(ckplus1[0:5])
     return ckplus1
 
 
 c1 = gen_c1()
 f1 = gen_fk(c1)
 f = f + f1
-print ("k=2")
+print("\nf1: " + str(len(f1)))
 c2 = gen_ckplus1(f1,0)
 f2 = gen_fk(c2)
 f = f + f2
 print("\nf2: " + str(len(f2)))
-print(f2[0:5])
-print ("\n\n\n\nk=3")
 c3 = gen_ckplus1(f2,0)
 f3 = gen_fk(c3)
 f = f + f3
 print("\nf3: " + str(len(f3)))
-print(f3[0:5])
-print ("\n\n\n\nk=4")
-c3 = gen_ckplus1(f3,0)
-f3 = gen_fk(c4)
-f = f + f4
-print("\nf4: " + str(len(f4)))
-print(f4[0:5])
+dump_f(f)
